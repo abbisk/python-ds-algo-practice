cf078c5147ca3969df201863f3e0ea36a19f8c51
abbisk <abbiskpro@gmail.com>
2023-06-30 18:21:18 +0530

    new problem added

---
+++ 2. Conditionals and Loops/Fahrenheit to Celsius.py
@@ -0,0 +1,2 @@
+n= int("Input the farenheit remp")
+
---
+++ 2. Conditionals and Loops/checkpalindrome.py
@@ -0,0 +1,31 @@
+# Write a program to determine if given number is palindrome or not. Print true if it is palindrome, false otherwise.
+# 1.Palindrome are the numbers for which reverse is exactly same as the original one. For eg. 121
+
+# Sample Input 1
+# 121
+
+# Sample Output 1 :
+# true
+
+# Sample Input 2
+# 1032
+
+# Sample Output 2
+# false
+# 
+
+def checkPalindrome(n):
+    reverse = 0
+    while n>0:
+        remainder=n%10
+        n= int(n/10)
+        reverse=reverse*10+remainder
+    return reverse
+
+
+n= int(input("Enter number to check palindrome: "))
+reverse = checkPalindrome(n)
+if n==reverse:
+    print("Palindrome")
+else:
+    print("Not palindrome")
